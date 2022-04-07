95152c65c0c2282fa451e28b70e725facc808880
GavinTetteroo <gavin_tetteroo@hotmail.com>
2022-04-07 11:46:44 +0200

    Heel veel CSS weg gegooit en zoekfunctie gemaakt
    dddd

--- js/main.js
+++ js/main.js
@@ -1,62 +1,43 @@
-const hamburger = document.querySelector("#hamburger")
-const sidebar = document.querySelector("#sidebar")
+const hamburger = document.querySelector("#hamburger");
+const sidebar = document.querySelector("#sidebar");
 
 hamburger.addEventListener("click", () => {
-    sidebar.classList.toggle("active")
-})
-
-const f = document.getElementById('form');
-      const q = document.getElementById('query');
-      
-
-      function submitted(event) {
-        event.preventDefault();
-        const url = '#' + q.value;  
-        const win = window.open (url, '_self  ');
-        win.focus();
-      }
-
-      f.addEventListener('submit', submitted);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-      const form = document.querySelector("#form");
-const query = document.querySelector("#query");
-const mailIndex = document.querySelector("#mailIndex");
-
-//popup windows
-mailIndex.addEventListener("click", () => {
-  window.open(
-    "contact.html",
-    "Google",
-    "width=400,height=425,scrollbars=no,toolbar=no,location=no"
-  );
+  sidebar.classList.toggle("active");
 });
 
-//Form/searchbar
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const url = "#" + query.value;
-  const win = window.open(url, "_self");
-  win.focus();
+const q = document.getElementById("query");
+const products = document.querySelectorAll(".product");
+const productenDiv = document.querySelector("#producten");
+
+q.addEventListener("input", (event) => {
+  productenDiv.innerHTML = "";
+
+  products.forEach((product) => {
+    const dataProduct = product.getAttribute("data-product");
+
+    if (dataProduct.indexOf(event.target.value) !== -1) {
+      productenDiv.appendChild(product);
+    }
+  });
 });
+
+// const form = document.querySelector("#form");
+// const query = document.querySelector("#query");
+// const mailIndex = document.querySelector("#mailIndex");
+
+// //popup windows
+// mailIndex.addEventListener("click", () => {
+//   window.open(
+//     "contact.html",
+//     "Google",
+//     "width=400,height=425,scrollbars=no,toolbar=no,location=no"
+//   );
+// });
+
+// //Form/searchbar
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const url = "#" + query.value;
+//   const win = window.open(url, "_self");
+//   win.focus();
+// });
